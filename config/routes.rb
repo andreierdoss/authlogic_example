40d5ade67d3fcc1482f4d6acab1d7d74537ea98e
Adam Anderson <adamandersonis@gmail.com>
2009-12-10 11:21:00 -0800

    Starting point for Part Two with refactored Javascript

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.root :controller => "home", :action => "index"
+  map.root :controller => "home"
+  map.connect ':controller/:action/:id'
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,41 +1,53 @@
-$(document).ready(function(){
+var MyApp = (function($){
   $.facebox.settings.loadingImage = '/javascripts/facebox/loading.gif';
   $.facebox.settings.closeImage = '/javascripts/facebox/closelabel.gif';
 
-  $('a[rel*=facebox]').click(function(){
-    var me = this;
-    $.facebox(function(){
-      $.get(me.href,
-            null,
-            function(data){
-              $.facebox(data);
-              ajaxifyLoginForm();
-            },
-            'html');
-    });
-    return false;
+  $().ajaxError(function(event, xhr, settings, error) {
+    switch(xhr.status) {
+      case 406:
+        $('#errorExplanation').remove();
+        $('#facebox form').prepend('<div id="errorExplanation" class="errorExplanation">\
+                                     <h2>Dang you got errored!</h2>\
+                                     <p>'+ xhr.responseText + '</p>\
+                                    </div>');
+        break;
+    }
   });
 
-  function ajaxifyLoginForm() {
-    $('#facebox .new_user_session').submit(function(){
-      var me = $(this);
-      $.ajax({
+  $(document).ready(function(){
+    $('a[rel*=facebox]').facebox();
+    $('a[href=/user_session/new]').click(function(){
+      MyApp.renderLoginForm();
+      return false;
+    });
+  });
+
+  return {
+    renderLoginForm: function(){
+      $.facebox(function(){
+        $.get('/user_session/new',
+              null,
+              function(data){
+                $.facebox(data);
+                MyApp.ajaxifyLoginForm();
+              },
+              'html');
+      });
+    },
+
+    ajaxifyLoginForm: function(){
+      $('#facebox .new_user_session').submit(function(){
+        var me = $(this);
+        $.ajax({
           type: 'POST',
           url: $(this).attr('action'),
           data: $(this).serialize(),
           dataType: 'html',
           success: function(data) { window.location = '/account'; },
-          error: function(xhr) {
-            if (xhr.status == '406') {
-              $('#errorExplanation').remove();
-              me.before('<div id="errorExplanation" class="errorExplanation">\
-                           <h2>Dang you got errored!</h2>\
-                           <p>'+ xhr.responseText + '</p>\
-                         </div>');
-            }
-          }
+          error: function(xhr) {}
+        });
+        return false;
       });
-      return false;
-    });
+    }
   }
-});
+})(jQuery);
